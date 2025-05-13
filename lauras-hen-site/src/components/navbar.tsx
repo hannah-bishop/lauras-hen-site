fd029dafce808d566a427a15733eec7898acb1e1
Hannah Bishop <hannah.bishop@prolific.com>
2025-05-13 19:56:52 +0100

    navbar

--- lauras-hen-site/src/components/navbar.tsx
+++ lauras-hen-site/src/components/navbar.tsx
@@ -1,9 +1,53 @@
-import React from 'react';
+import React, { useState }  from 'react';
+import { Link } from "react-router-dom";
+import "./navbar.css"; // Import the CSS for styling
 
-export const Navbar: React.FC = () => {
+export const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  // Toggle the menu
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <div>
-      <p>Hello!</p>
-    </div>
+    <nav className="navbar">
+      <div className="navbar-container">
+        <Link to="/" className="navbar-logo">
+          MyLogo
+        </Link>
+
+        {/* Burger Icon */}
+        <div className="burger-icon" onClick={toggleMenu}>
+          <div className={`bar ${isMenuOpen ? "open" : ""}`}></div>
+          <div className={`bar ${isMenuOpen ? "open" : ""}`}></div>
+          <div className={`bar ${isMenuOpen ? "open" : ""}`}></div>
+        </div>
+
+        {/* Menu items */}
+        <ul className={`nav-links ${isMenuOpen ? "active" : ""}`}>
+          <li>
+            <Link to="/" onClick={() => setIsMenuOpen(false)}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/about" onClick={() => setIsMenuOpen(false)}>
+              About
+            </Link>
+          </li>
+          <li>
+            <Link to="/services" onClick={() => setIsMenuOpen(false)}>
+              Services
+            </Link>
+          </li>
+          <li>
+            <Link to="/contact" onClick={() => setIsMenuOpen(false)}>
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 };
