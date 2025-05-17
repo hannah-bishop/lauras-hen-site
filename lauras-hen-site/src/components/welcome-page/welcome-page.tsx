cb165766356097543291f6c199b4c6e7231d38bf
Hannah Bishop <hannah.bishop@prolific.com>
2025-05-17 11:09:07 +0100

    make stylish

--- lauras-hen-site/src/components/welcome-page/welcome-page.tsx
+++ lauras-hen-site/src/components/welcome-page/welcome-page.tsx
@@ -4,9 +4,8 @@ import { Link } from "react-router-dom";
 export const WelcomePage: React.FunctionComponent = () => {
   return (
     <div className="header">
-        <h1 className="welcome">Welcome</h1>
         <Link to="/home">
-            <img src="clicktoenter.JPG" className="logo"></img>
+            <img src={require("public/welcome-logo.png")} className="logo" alt="logo"></img>
         </Link>
     </div>
   );
