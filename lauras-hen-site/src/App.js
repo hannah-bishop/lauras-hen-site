75788effc367d1f94a2a4d11b195d85cd7d2d83f
Hannah Bishop <hannah.bishop@prolific.com>
2025-06-14 10:29:19 +0100

    itinerary

--- lauras-hen-site/src/App.js
+++ lauras-hen-site/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import { WelcomePage } from "./components/welcome-page/welcome-page.tsx";
+import { Itinerary } from 'components/itinerary/itinerary';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/lauras-hen-site/" element={<WelcomePage />} />
+          <Route path="/lauras-hen-site/itinerary" element={<Itinerary />} />
         </Routes>
       </Router>
     </div>
--- lauras-hen-site/src/components/welcome-page/welcome-page.tsx
+++ lauras-hen-site/src/components/welcome-page/welcome-page.tsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 
 export const WelcomePage: React.FunctionComponent = () => {
   return (
-    <div className="header">
-        <Link to="/home">
-            <img src={require("public/welcome-logo.png")} className="logo" alt="logo"></img>
+    <div>
+        <Link to="/lauras-hen-site/itinerary">
+            <img src={require("assets/welcome-logo.png")} className="logo-home" alt="logo"></img>
         </Link>
     </div>
   );
