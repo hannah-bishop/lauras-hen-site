27524452bc500aaf08744a03e764f7e08e6df422
Hannah Bishop <hannah.bishop@prolific.com>
2025-07-27 12:16:20 +0100

    hash routing

--- lauras-hen-site/src/App.js
+++ lauras-hen-site/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 import { WelcomePage } from "./components/welcome-page/welcome-page.tsx";
 import { Itinerary } from "./components/itinerary/itinerary.tsx";
