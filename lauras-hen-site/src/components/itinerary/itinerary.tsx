e9b1c7fb57fefc18a85469c7d29d2db11db27f3e
Hannah Bishop <hannah.bishop@prolific.com>
2025-07-29 22:18:02 +0100

    more links

--- lauras-hen-site/src/components/itinerary/itinerary.tsx
+++ lauras-hen-site/src/components/itinerary/itinerary.tsx
@@ -30,17 +30,33 @@ export const Itinerary: React.FunctionComponent = () => {
         <p className="list-item">6pm: Carraige into town</p>
         <p className="list-item">
           6.30pm: Drinks and Hooch pong at{" "}
-          <a href="https://www.cheapdatesbar.co.uk/" className="link">
+          <a
+            href="https://www.welcometosheffield.co.uk/content/food-drink-and-nightlife/cheap-dates/"
+            className="link"
+          >
             Cheap Dates
           </a>
         </p>
-        <p className="list-item">9pm: Karaoke</p>
+        <p className="list-item">
+          9pm: Karaoke{" "}
+          <a href="https://www.karaoke-sheffield.co.uk/" className="link">
+            K Max
+          </a>
+        </p>
         <p className="list-item">
           11pm: Boogie at the club (if still standing)
         </p>
         <h2 className="underline">Sunday</h2>
         <p className="list-item">10am: Check-out</p>
-        <p className="list-item">10.30am: Brunch</p>
+        <p className="list-item">
+          10.30am: Brunch at{" "}
+          <a
+            href="https://www.canva.com/design/DAE8Sz9Q1sc/Rn7d9-Td0lKaQbsET1xhHA/view?website#4:drinks"
+            className="link"
+          >
+            Kollective Kitchen
+          </a>
+        </p>
         <p className="list-item">12pm: Home time :(</p>
       </div>
     </div>
